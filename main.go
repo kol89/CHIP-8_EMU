07fefc6be0690b6c6f909fb0c493e400c6368f1b
Aleksandr <afominovs@gmail.com>
2026-09-13 17:31:36 +0300

    Added new cpu instructions and input handler

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"log"
 	"os"
 	"fmt"
 	"time"
 	"slices"
+	"math/rand/v2"
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
 	//"encoding/hex"
 	//"encoding/binary"
 )
@@ -12,13 +16,15 @@ import (
 
 var (
 	//Metadata
-	cpu_speed float32 = 0.0007 //counted in MHz
+	behavior string = "old" // desides if before the Shift command VX would be set or not (old - YES/new - NO)
+	cpu_speed float32 = 1 //counted in MHz
 
 	// Registers
 	reg_I uint16 // index register
 	sound_timer uint8 // sound timer register
 	delay_timer uint8 // delay timer register
 	registers []byte = make([]byte, 16)//general-purpose registers v0-vf
+	reg_dump []byte = make([]byte, 16)
 
 	//Memory
 	rom []byte = make([]byte, 1024) // rom data buffer
@@ -43,6 +49,7 @@ var (
 		0xF0, 0x80, 0xF0, 0x80, 0x80} // F
 	display [32][64]bool // Display 64x32 monochrome pixels
 	stack []byte // stack
+	keypad []bool = make([]bool, 16) // keypad data
 
 	//Other
 	ram_dump []byte = make([]byte, 4096)
@@ -51,7 +58,71 @@ var (
 	PC int // opcode pointer
 	rom_name = "IBM.ch8" // name of executable rom
 	counter int
+	sprite byte // container for the sprite data
+	pixel bool
 )
+
+func input_handler(){
+	for{
+		fmt.Println("aaa")
+		err := keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+
+			if key.Code == keys.Escape || key.Code == keys.CtrlC {
+				return true, nil // Возвращаем true, чтобы остановить прослушивание
+			}
+			for i := range keypad {
+				keypad[i] = false
+			}
+			//fmt.Println(key.String())
+			switch(key.String()){
+				case "1":
+					keypad[0] = true
+				case "2":
+					keypad[1] = true
+				case "3":
+					keypad[2] = true
+				case "4":
+					keypad[12] = true
+
+				case "q":
+					keypad[3] = true
+				case "w":
+					keypad[4] = true
+				case "e":
+					keypad[5] = true
+				case "r":
+					keypad[13] = true
+
+				case "a":
+					keypad[6] = true
+				case "s":
+					keypad[7] = true
+				case "d":
+					keypad[8] = true
+				case "f":
+					keypad[14] = true
+				
+				case "z":
+					keypad[9] = true
+				case "x":
+					keypad[10] = true
+				case "c":
+					keypad[11] = true
+				case "v":
+					keypad[15] = true
+				
+				default:
+					for i := range keypad {
+						keypad[i] = false
+					}
+			}
+			return false, nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
 
 func cpu(opcode uint16){
 	switch{
@@ -72,29 +143,89 @@ func cpu(opcode uint16){
 			PC = int(stack[len(stack)-1])
 			stack = stack[:len(stack)-1]
 			PC-=2
-		case opcode>>12 == 0x3: // 0x3XNN skip one instructrion if vX == NN
+		case opcode>>12 == 0x3: // 0x3XNN skip one instructrion if VX == NN
 			if registers[opcode>>8 & 0x0F] == byte(opcode & 0x00FF){
 				PC+=2
 			}
-		case opcode>>12 == 0x5: // 0x5XY0 skip one instructrion if vX == vY
-			if registers[opcode>>8 & 0x0F] == byte(opcode>>4 & 0x00F){
+		case opcode>>12 == 0x5: // 0x5XY0 skip one instructrion if VX == VY
+			if registers[opcode>>8 & 0x0F] == registers[opcode>>4 & 0x00F]{
 				PC+=2
 			}
 		
-		case opcode>>12 == 0x6: // 0x6XNN set vX register to the value NN
+		case opcode>>12 == 0x6: // 0x6XNN set VX register to the value NN
 			registers[opcode>>8 & 0x0F] = byte(opcode & 0x00FF)
-		case opcode>>12 == 0x7: // 0x7XNN adds value NN to the vX register
+		case opcode>>12 == 0x7: // 0x7XNN adds value NN to the VX register
 			if registers[opcode>>8 & 0x0F] + byte(opcode & 0x00FF) > 0xFF{
 				registers[opcode>>8 & 0x0F] = 0xFF
 			}else{
 				registers[opcode>>8 & 0x0F] += byte(opcode & 0x00FF)
 			}
-		case opcode>>12 == 0x9: // 0x5XY0 skip one instructrion if vX != vY
+		case opcode>>12 == 0x8: // Logical and arithmetic instructions
+			switch(opcode & 0x000F){
+				case 0x0: // Set
+					registers[opcode>>8 & 0x0F] = registers[opcode>>4 & 0x00F]
+				case 0x1: // Binary OR
+					registers[opcode>>8 & 0x0F] = registers[opcode>>8 & 0x0F] | registers[opcode>>4 & 0x00F]
+				case 0x2: // Binary AND
+					registers[opcode>>8 & 0x0F] = registers[opcode>>8 & 0x0F] & registers[opcode>>4 & 0x00F]
+				case 0x3: // Logical XOR
+					registers[opcode>>8 & 0x0F] = registers[opcode>>8 & 0x0F] ^ registers[opcode>>4 & 0x00F]
+				case 0x4: // Add
+					registers[opcode>>8 & 0x0F] += registers[opcode>>4 & 0x00F]
+				case 0x5: // sets VX to the result of VX - VY
+					registers[opcode>>8 & 0x0F] -= registers[opcode>>4 & 0x00F]
+				case 0x6: // Shift right
+					if behavior == "old"{
+						registers[opcode>>8 & 0x0F] = registers[opcode>>4 & 0x00F]
+					}
+					registers[opcode>>8 & 0x0F] = registers[opcode>>8 & 0x0F]>>1
+				case 0x7: // sets VX to the result of VY - VX
+					registers[opcode>>8 & 0x0F] = registers[opcode>>4 & 0x00F] - registers[opcode>>8 & 0x0F]
+				case 0xE: // Shift left
+					if behavior == "old"{
+						registers[opcode>>8 & 0x0F] = registers[opcode>>4 & 0x00F]
+					}
+					registers[opcode>>8 & 0x0F] = registers[opcode>>8 & 0x0F]<<1
+			}
+			 
+		case opcode>>12 == 0x9: // 0x5XY0 skip one instructrion if VX != VY
 			if registers[opcode>>8 & 0x0F] != byte(opcode>>4 & 0x00F){
 				PC+=2
 			}
-		case opcode>>12 == 0xA: // Sets I register
+		case opcode>>12 == 0xA: // Sets I (index) register
 			reg_I = opcode & 0x00FF
+		case opcode>>12 == 0xB: // 0xBNNN Jump with offset
+			if behavior == "old"{
+				PC = int(opcode & 0x0FFF + uint16(registers[0]))
+			}else{
+				PC = int(opcode & 0x0FFF + uint16(registers[opcode>>8 & 0x0F]))
+			}
+			PC-=2
+		case opcode>>12 == 0xC: // 0xCXNN Random
+			registers[opcode>>8 & 0x0F] = byte(rand.Uint32()) & byte(opcode & 0x00FF)
+		case opcode>>12 == 0xD: // 0xDXYN Display WIP
+			X := registers[opcode>>8 & 0x0F] & 63
+			Y := registers[opcode>>4 & 0x00F] & 31
+			registers[len(registers)-1] = 0
+			for i := 0; i <= int(opcode & 0x000F); i++{
+				sprite = ram[reg_I + (opcode & 0x000F)]
+				for j := 0; j <= 8; j++{
+					pixel = (sprite>>(7-j))&1 == 1
+					if X>64{
+						break
+					}
+					if pixel{ 
+						display[Y][X]=!display[Y][X]
+						registers[len(registers)-1]=1 
+					}
+					X++
+				}
+				Y++
+				if Y>32{
+					break
+				}
+			}
+		case opcode>>12 == 0xF:
 	}
 }
 
@@ -144,9 +275,10 @@ func loop() {
 	cycleDuration := time.Second / time.Duration(cpu_speed*1_000_000)
 	ticker := time.NewTicker(cycleDuration)
 	defer ticker.Stop()
-
+	go input_handler()
 	for range ticker.C {
 		ram_dump = ram
+		reg_dump = registers
 		PC+=2
 		counter++
 		if PC >= 1536{
@@ -154,12 +286,22 @@ func loop() {
 		}
 		opcode = (uint16(ram[PC+1]) << 8) | uint16(ram[PC]) // getting curent opcode
 		cpu(opcode)
-		if !slices.Equal(ram, ram_dump) || display_dump!=display{
+		if !slices.Equal(ram, ram_dump) || !slices.Equal(registers, reg_dump){
+			fmt.Println(ram)
 			render(display)
 		}
 		//fmt.Println(ram[PC], "/", ram[PC+1])
 		if counter==700{
 			counter = 0
+			fmt.Println("\n")
+			for i := range keypad {
+			if keypad[i]{
+				fmt.Print("1")
+			}else{
+				fmt.Print("0")
+			}
+			}
+			//render(display)
 			//fmt.Println("////")
 			//fmt.Println(ram)
 		}
